49d5d0ab557e551dc3260007eeb47e56e9088512
Corbin Koukoutchos <ckoukoutchos@gmail.com>
2019-01-19 18:30:50 -0800

    re-did corner buttons

--- src/components/card/title-card/TitleCard.js
+++ src/components/card/title-card/TitleCard.js
@@ -4,18 +4,20 @@ import classes from './TitleCard.module.css';
 import Button from '../../button/Button';
 
 const titleCard = props => {
-  const { editing, onEdit, title } = props;
+  const { editing, onEdit, readOnly, title } = props;
   return (
     <div className={classes.Card}>
-      <div className={classes.Header}>
+      <div className={readOnly ? classes.Header : classes.HeaderEdit}>
         <h1 className={classes.Title}>{title}</h1>
-        <Button
-          color={editing ? 'Secondary' : 'Primary'}
-          btnType="Corner"
-          clicked={onEdit}
-        >
-          {editing ? 'x' : '+'}
-        </Button>
+        {readOnly ? null : (
+          <Button
+            color={editing ? 'Secondary' : 'Primary'}
+            btnType="Corner"
+            clicked={onEdit}
+          >
+            {editing ? 'x' : '+'}
+          </Button>
+        )}
       </div>
       <div className={classes.Body}>{props.children}</div>
     </div>
--- src/containers/track/Track.js
+++ src/containers/track/Track.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import Button from '../../components/button/Button';
 import Input from '../../components/input/Input';
 import Jumbotron from '../../components/jumbotron/Jumbotron';
 import TitleCard from '../../components/card/title-card/TitleCard';
@@ -28,79 +27,11 @@ class Track extends Component {
           />
           <Input label="Race" value={this.state.race} />
           <Input label="Alignment" value={this.state.alignment} />
-          <div
-            style={{
-              display: 'flex',
-              margin: '8px 16px',
-              justifyContent: 'space-around',
-              width: '100%',
-              alignItems: 'center'
-            }}
-          >
-            <div
-              style={{
-                height: '2px',
-                width: '180px',
-                background: 'rgb(149, 149, 149)'
-              }}
-            />
-            <Button
-              color="Primary"
-              btnType="Raised"
-              clicked={this.onHeroSelection}
-            >
-              View
-            </Button>
-            <Button color="Secondary" btnType="Raised">
-              Delete
-            </Button>
-            <div
-              style={{
-                height: '2px',
-                width: '180px',
-                background: 'rgb(149, 149, 149)'
-              }}
-            />
-          </div>
         </TitleCard>
         <TitleCard title="Valarian" readOnly>
           <Input label={'Class & Level'} value="Ranger 1" />
           <Input label="Race" value="Elf" />
           <Input label="Alignment" value="Chaotic Good" />
-          <div
-            style={{
-              display: 'flex',
-              margin: '8px 16px',
-              justifyContent: 'space-around',
-              width: '100%',
-              alignItems: 'center'
-            }}
-          >
-            <div
-              style={{
-                height: '2px',
-                width: '180px',
-                background: 'rgb(149, 149, 149)'
-              }}
-            />
-            <Button
-              color="Primary"
-              btnType="Raised"
-              clicked={this.onHeroSelection}
-            >
-              View
-            </Button>
-            <Button color="Secondary" btnType="Raised">
-              Delete
-            </Button>
-            <div
-              style={{
-                height: '2px',
-                width: '180px',
-                background: 'rgb(149, 149, 149)'
-              }}
-            />
-          </div>
         </TitleCard>
       </>
     );
