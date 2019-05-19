ccd29f12604bfb697fb9a95e690b1644693096b2
Corbin Koukoutchos <ckoukoutchos@gmail.com>
2019-05-19 14:03:58 -0700

    refactored navbar

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -5,88 +5,31 @@ import { AppState } from '../../store/rootReducer';
 
 import classes from './NavBar.module.css';
 import NavItem from './nav-items/NavItem';
+import { User } from '../../models/User';
 
 interface NavBarProps {
-  user: any;
+  user: User | null;
 }
 
-interface NavBarState {
-  menuToggle: boolean;
-}
-
-// TODO: fix wonky resolution issues
-
-class NavBar extends Component<any, NavBarState> {
-  state = {
-    menuToggle: false
-  };
-
-  /**
-   * @name onMenuToggle
-   * @description toogles menu open/closed state
-   */
-  onMenuToggle = (): void => {
-    this.setState((prevState: NavBarState) => {
-      return { menuToggle: !prevState.menuToggle };
-    });
-  };
-
+class NavBar extends Component<NavBarProps> {
   render() {
-    return (
+    let navBar = (
       <header className={classes.NavBar}>
-        <div className={classes.Logo}>
-          <NavItem link="/" exact>
-            Dragonborn
-          </NavItem>
-        </div>
-
-        {this.props.user ? (
-          <div>
-            <nav className={classes.Links}>
-              <NavItem link="/create">Create</NavItem>
-              <NavItem link="/track">Track</NavItem>
-              <NavItem link="/guide">Guide</NavItem>
-              <NavItem link="/profile">Profile</NavItem>
-              <NavItem link="/logout">Logout</NavItem>
-            </nav>
-            <div className={classes.Menu} onClick={this.onMenuToggle}>
-              {this.state.menuToggle ? 'Close' : 'Menu'}
-            </div>
-          </div>
-        ) : (
-          <div>
-            <nav className={classes.Links}>
-              <NavItem link="/login">Login</NavItem>
-            </nav>
-            <div className={classes.Menu}>
-              <NavItem link="/login">Login</NavItem>
-            </div>
-          </div>
-        )}
-
-        {this.state.menuToggle ? (
-          <div className={classes.MenuContainer} onClick={this.onMenuToggle}>
-            <nav>
-              <NavItem link="/create" menu>
-                Create
-              </NavItem>
-              <NavItem link="/track" menu>
-                Track
-              </NavItem>
-              <NavItem link="/guide" menu>
-                Guide
-              </NavItem>
-              <NavItem link="/profile" menu>
-                Profile
-              </NavItem>
-              <NavItem link="/logout" menu>
-                LogOut
-              </NavItem>
-            </nav>
-          </div>
-        ) : null}
+        <NavItem link="/">Dragonborn</NavItem>
+        <NavItem link="/login">Login</NavItem>
       </header>
     );
+
+    if (this.props.user) {
+      navBar = (
+        <header className={classes.NavBar}>
+          <NavItem link="/dashboard">Dashboard</NavItem>
+          <NavItem link="/logout">Logout</NavItem>
+        </header>
+      );
+    }
+
+    return navBar;
   }
 }
 
